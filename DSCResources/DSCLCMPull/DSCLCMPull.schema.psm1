83b53dbe95b81fbca8f0bd349ae5b0e981a60c33
Danny den Braver <danny@denbraver.com>
2016-04-22 01:34:27 +0200

    added pullserver as an option

---
+++ DSCResources/DSCLCMPull/DSCLCMPull.schema.psm1
@@ -0,0 +1,21 @@
+﻿[DSCLocalconfigurationmanager()]
+configuration DSCLCMPull
+{
+    Node $Allnodes.NodeName
+    {
+        Settings 
+        {
+            RefreshMode                    = 'PULL'
+            RebootNodeIfNeeded             = $true
+            ActionAfterReboot              = 'ContinueConfiguration'
+            RefreshFrequencyMins           = 30
+            ConfigurationModeFrequencyMins = 30 
+            ConfigurationMode              = 'ApplyAndAutoCorrect'
+        }
+
+        ConfigurationRepositoryWeb Pull
+	    {
+		    ServerURL               = $Node.Pullserverurl
+        }
+    }
+}
